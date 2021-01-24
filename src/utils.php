b51402c1eb958388476559b96fe4e0e67ea0d646
datagutten <datagutten@datagutten.net>
2021-01-24 11:33:13 +0100

    Add method and script to create torrent file

--- src/utils.php
+++ src/utils.php
@@ -4,6 +4,8 @@
 namespace torrentupload;
 
 
+use PHP\BitTorrent\Torrent;
+
 class utils
 {
 
@@ -14,5 +16,26 @@ class utils
                 $array[$key] = utf8_decode($value);
         }
         return $array;
+    }
+
+    /**
+     * @param string $path Path to create torrent from
+     * @param string $torrent_file File name for the torrent file
+     * @param string $tracker Tracker URL
+     * @param integer $piece_length Piece length exponent
+     * @return string The created torrent file
+     */
+    public static function create_torrent(string $path, string $torrent_file, $tracker='http://localhost', $piece_length = null): string
+    {
+        if (!file_exists($torrent_file))
+        {
+            $torrent = Torrent::createFromPath($path, $tracker);
+            if(!empty($piece_length))
+                $torrent->setPieceLengthExp($piece_length);
+            $torrent->save($torrent_file);
+        }
+        else
+            echo "Torrent is already created\n";
+        return $torrent_file;
     }
 }
